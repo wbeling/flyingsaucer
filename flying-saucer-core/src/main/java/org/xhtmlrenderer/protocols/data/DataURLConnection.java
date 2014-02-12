d546a12a80002d3b95a4a87e88d8382f74e5280f
Dan Fickle <newtab@gmail.com>
2014-02-12 13:27:13 +1100

    Replaced home made base64 decoder with commons codec implementation.

--- flying-saucer-core/src/main/java/org/xhtmlrenderer/protocols/data/DataURLConnection.java
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/protocols/data/DataURLConnection.java
@@ -171,50 +171,8 @@ class URLByteDecoder {
 
 class Base64 {
     
-    private static String _map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    
-    public static byte [] decode(String s) {
-
-        if (s == null || s.length() < 4)
-            return new byte [] {};
-        
-        s = s.replaceAll("[^A-Za-z0-9\\+\\/\\=]+", "");
-        
-        if (s.length() < 4)
-            return new byte [] {};
-        
-        int padding = 0;
-
-        if (s.charAt(s.length() - 1) == '=')
-            padding++;
-        if (s.charAt(s.length() - 2) == '=')
-            padding++;
-
-        byte [] input = s.getBytes();
-        byte [] output = new byte [((s.length() / 4) * 3) - padding];
-        
-        int outputIndex = 0;
-
-        for (int i = 0; i < input.length; i += 4) {
-
-            int e =
-                _map.indexOf(input[i])     << 18 |
-                _map.indexOf(input[i + 1]) << 12 |
-                _map.indexOf(input[i + 2]) << 6  |
-                _map.indexOf(input[i + 3]);
-            
-            output[outputIndex++] = (byte) ((e >> 16) & 0xFF);
-            
-            if (input[i + 2] != '=') {
-                output[outputIndex++] = (byte) ((e >> 8)  & 0xFF);
-
-                if (input[i + 3] != '=') {
-                    output[outputIndex++] = (byte) (e & 0xFF);
-                }
-            }
-        }
-
-        return output;
+    public static byte [] decode(String s) 
+    {
+    	return s == null ? new byte[0] : org.apache.commons.codec.binary.Base64.decodeBase64(s);
     }
-
 }
